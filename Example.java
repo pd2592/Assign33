8d5f57db3b13b8092a23b1cb580c09c96d58e647
Priyanka <priyanka.rupali@gmail.com>
2017-01-05 11:57:20 +0530

    The program will print pnt is null 0 0

---
+++ Example.java
@@ -0,0 +1,9 @@
+package assignment.acadgild33;
+
+class Example {
+	 int x; int y;
+	 String name;
+	 public static void main(String args[]) {
+	 Example pnt = new Example();
+	 System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+	} }
